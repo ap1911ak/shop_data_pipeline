9b6b250ae3ceb2facd5c1df078a54b38e216cc3c
CrocodileSoy <ap1911ak@gmail.com>
2026-06-28 21:10:19 +0700

    add loging in extract.py

--- src/extract.py
+++ src/extract.py
@@ -1,13 +1,15 @@
 import sqlite3
 import pandas as pd
-from prefect import task
+from prefect import task, get_run_logger
 
 @task(name="Extract Table Data")
 def extract_table(conn: sqlite3.Connection, table_name: str) -> pd.DataFrame:
+    logger = get_run_logger()
+    logger.info(f"Extracting data from table: {table_name}")
     try:
         query = f"SELECT * FROM {table_name}"
         df = pd.read_sql_query(query, conn)
         return df
     except pd.errors.DatabaseError as e:
-        raise(f"Not found : '{table_name}': {e}")
-        
+        logger.error(f"Not found : '{table_name}': {e}")
+        raise
